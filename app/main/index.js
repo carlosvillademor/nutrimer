ce69a832c42f623115e4998b7db8cd35aa086677
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2015-05-03 13:55:43 +0100

    Seeting port property

--- app/main/index.js
+++ app/main/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(path.resolve(__dirname + '/../../dist')));
 app.use(express.static(path.resolve(__dirname + '/../../bower_components')));
 
@@ -9,4 +11,6 @@ app.get('/', function(req, res){
   res.sendFile(path.resolve(__dirname + '/../../dist/index.html'));
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running on port", app.get('port'))
+})
