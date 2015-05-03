4c67f19f53ea99961063a1e80ca755212c1bc47c
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2015-05-03 12:52:11 +0100

    Adding static paths

--- app/main/index.js
+++ app/main/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(path.resolve(__dirname + '/../../dist')));
+app.use(express.static(path.resolve(__dirname + '/../../bower_components')));
+
 app.get('/', function(req, res){
-  res.sendFile(path.resolve(__dirname + '/../index.html'));
+  res.sendFile(path.resolve(__dirname + '/../../dist/index.html'));
 });
 
 app.listen(5000);
